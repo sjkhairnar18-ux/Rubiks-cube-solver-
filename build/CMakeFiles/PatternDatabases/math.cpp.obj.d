4172addd558769206215b594d0df33dc8852ef4b
sjkhairnar18-ux <s.j.khairnar18@gmail.com>
2026-07-20 17:43:01 +0530

    Add files via upload

---
+++ build/CMakeFiles/PatternDatabases/math.cpp.obj.d
@@ -0,0 +1,18 @@
+CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/math.cpp.obj: \
+ C:\Users\23ish\ProgrammingStuff\Projects\rubiksCube\SolveTheCube\PatternDatabases\math.cpp \
+ C:\Users\23ish\ProgrammingStuff\Projects\rubiksCube\SolveTheCube\PatternDatabases\math.h \
+ C:/mingw64/include/c++/15.2.0/cstdint \
+ C:/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/c++config.h \
+ C:/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/os_defines.h \
+ C:/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/cpu_defines.h \
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ C:/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stddef.h
